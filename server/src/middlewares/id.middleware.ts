98e2f98afcd6eaf6edabe9ecda643c16a182f0f9
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-25 10:19:45 +0300

    ref: split middlewares

--- server/src/middlewares/id.middleware.ts
+++ server/src/middlewares/id.middleware.ts
@@ -3,7 +3,6 @@ import { TypedRequestParams } from "../interfaces/IRequest";
 import { HttpError } from "../utils/notes.errors";
 import mongoose from "mongoose";
 import { StatusCodes } from "http-status-codes";
-import notesService from "../notes.service";
 
 const idMiddleware = async (
   req: TypedRequestParams<{ id: string }>,
@@ -17,15 +16,6 @@ const idMiddleware = async (
         httpCode: StatusCodes.BAD_REQUEST,
         message: "There is no id in params",
       });
-
-    const note = await notesService.getOne(req.params.id);
-    if (!note) {
-      throw new HttpError({
-        name: "Not found",
-        httpCode: StatusCodes.NOT_FOUND,
-        message: "Can't find note with such id",
-      });
-    }
     next();
   } catch (error) {
     next(error);
@@ -33,9 +23,3 @@ const idMiddleware = async (
 };
 
 export { idMiddleware };
-
-/*        new HttpError({
-          name: "Not found",
-          httpCode: StatusCodes.NOT_FOUND,
-          message: "Can't find note with such id",
-        })*/
